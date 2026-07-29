16e88ca35d931b9f4e100f8df17a6e85414ea960
axmedovadalet3-stack <axmedovadalet3@gmail.com>
2026-07-29 14:54:44 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+# Telegram Bot Token (BotFather-dan aldığın token)
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "BURAYA_TOKEN_YAZILACAQ")
+
+# Google Gemini API Kaliti
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "BURAYA_GEMINI_KEY_YAZILACAQ")
